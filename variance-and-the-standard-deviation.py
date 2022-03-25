18c6d8fc40663fc41bc0389a95a876ebff24deac
ygsjason <99283954+ygsjason@users.noreply.github.com>
2022-03-25 11:56:47 -0500

    Update variance-and-the-standard-deviation.py

--- variance-and-the-standard-deviation.py
+++ variance-and-the-standard-deviation.py
@@ -11,3 +11,10 @@ variance = ((df2['score'] - df2['score'].mean())**2).mean()
 deviation = variance**0.5
 
 df3 = pd.DataFrame({'variance': [variance], 'std_deviation' : [deviation]})
+
+#method 2 using built-in Pandas Statistical Methods
+var = df[df['grade']=='A']['score'].var(ddof = 0)
+
+stdev = df[df['grade'] == 'A']['score'].std(ddof = 0)
+
+df4 = pd.DataFrame({'var': [var], stdev : [stdev]})
